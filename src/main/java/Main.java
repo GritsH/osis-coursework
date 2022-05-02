8a354070766510597d7f0d59a349a897d02e528c
Helen <grits.lena@gmail.com>
2022-05-02 15:48:05 +0300

    add video compressor

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import by.grits.videoCompression.VideoCompressor;
+import io.github.techgnious.exception.VideoException;
+import main.java.by.grits.imageCompression.LossyCompressor;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException, VideoException {
+        Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8);
+        System.out.println("Enter path to the image: ");
+        String path = scanner.nextLine();
+//        LossyCompressor lossyCompressor = new LossyCompressor();
+//        lossyCompressor.compressImage(path);
+        VideoCompressor videoCompressor = new VideoCompressor();
+        videoCompressor.compressVideo(path);
+    }
+}
---
+++ src/main/java/by/grits/videoCompression/VideoCompressor.java
@@ -0,0 +1,23 @@
+package by.grits.videoCompression;
+
+import io.github.techgnious.IVCompressor;
+import io.github.techgnious.dto.IVSize;
+import io.github.techgnious.dto.ResizeResolution;
+import io.github.techgnious.dto.VideoFormats;
+import io.github.techgnious.exception.VideoException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class VideoCompressor {
+
+    public void compressVideo(String videoPath) throws IOException, VideoException {
+        IVCompressor compressor = new IVCompressor();
+        IVSize customRes = new IVSize();
+        customRes.setWidth(400);
+        customRes.setHeight(300);
+        File file = new File(videoPath);
+        compressor.reduceVideoSizeAndSaveToAPath(file, VideoFormats.MP4, ResizeResolution.R480P, "D:/Testing/Custome");
+    }
+
+}
